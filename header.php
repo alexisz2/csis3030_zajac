6a75e0f7faef90093c4869e5b06b8de164c6ead6
Alexis Zajac <azajac01@wildcats.jwu.edu>
2016-02-05 14:53:00 -0500

    Css
    added minimal css

--- header.php
+++ header.php
@@ -10,31 +10,17 @@
   <div id="content">
     <div id="header">
       <div id="logo">
-        <h1>Restaurant Menu</h1>
-        <h4>view the restaurant menu</h4>
+        <h1>Ecommerce Site</h1>
+        <h4>purchase the items you need</h4>
       </div>
+      
       <div id="links">
         <ul>
           <li><a href="index.php">Home</a></li>
+           <li><a href="category_list.php">Category List</a></li>
 
          
-             <?php
 
-  //if the person is logged in, show link to logout
-  if ($_SESSION["id"] != "") {
-    
-     echo "<li><a href='admin_menu_list.php'>Menu List</a></li>";
-    echo "<li><a href='admin_logout.php'>Logout</a></li>";
-    
-
-    
-  } else {
-  
-    //else, show link to login
-    echo "<li><a href='admin_login_form.php'>Login</a></li>";
-    
-  }
-  ?>
          
         
           
--- index.php
+++ index.php
@@ -6,7 +6,9 @@ include("global.php");
 include("header.php");
 ?>
 
-<a href="category_list.php">Category List</a>
+<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+</p>
 
 
 
